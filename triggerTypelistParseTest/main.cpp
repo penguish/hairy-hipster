753e473873cd563336fce933019df3b6c8ee2a3a
sroe <sroe@Macintosh.local>
2015-02-15 12:08:58 +0100

    Initial Commit

---
+++ triggerTypelistParseTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  triggerTypelistParseTest
+//
+//  Created by sroe on 15/02/2015.
+//  Copyright (c) 2015 sroe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
